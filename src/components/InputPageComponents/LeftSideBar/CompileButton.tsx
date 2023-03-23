723a9b0f8105600fb1fe42b27440cc320265947c
DanYu12 <dan.daniel.yu@gmail.com>
2023-03-22 22:08:53 -0400

    changed port

--- src/components/InputPageComponents/LeftSideBar/CompileButton.tsx
+++ src/components/InputPageComponents/LeftSideBar/CompileButton.tsx
@@ -25,7 +25,7 @@ const CompileButton = () => {
     });
     const output = { Edges: outputEdges, Nodes: outputNodes };
     axios
-      .post('https://127.0.0.1:5000', {
+      .post('http://127.0.0.1:5000', {
         graph: output,
       })
       .then((res) => {
