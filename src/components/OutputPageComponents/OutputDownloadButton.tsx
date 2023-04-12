f808af22c5611a398b6b001e5fe205d0fa5cb23b
DanYu12 <dan.daniel.yu@gmail.com>
2023-04-11 20:01:16 -0400

    a

--- src/components/OutputPageComponents/OutputDownloadButton.tsx
+++ src/components/OutputPageComponents/OutputDownloadButton.tsx
@@ -1,13 +1,13 @@
 import { Button } from '@mui/material';
 import { useOutletContext } from 'react-router-dom';
-import { useScreenshot, createFileName } from 'use-react-screenshot';
+// import { useScreenshot, createFileName } from 'use-react-screenshot';
 
 const OutputDownloadButton = ({ screenshotRef }: any) => {
   const { output }: any = useOutletContext();
-  const [image, takeScreenShot] = useScreenshot({
-    type: 'image/jpeg',
-    quality: 1.0,
-  });
+  // const [image, takeScreenShot] = useScreenshot({
+  //   type: 'image/jpeg',
+  //   quality: 1.0,
+  // });
 
   const exportData = () => {
     const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
@@ -20,13 +20,13 @@ const OutputDownloadButton = ({ screenshotRef }: any) => {
     link.download = 'data.json';
     link.click();
 
-    const download = (image: any, { name = 'img', extension = 'jpg' } = {}) => {
-      const a = document.createElement('a');
-      a.href = image;
-      a.download = createFileName(extension, name);
-      a.click();
-    };
-    takeScreenShot(screenshotRef.current).then(download);
+    // const download = (image: any, { name = 'img', extension = 'jpg' } = {}) => {
+    //   const a = document.createElement('a');
+    //   a.href = image;
+    //   a.download = createFileName(extension, name);
+    //   a.click();
+    // };
+    // takeScreenShot(screenshotRef.current).then(download);
   };
 
   return <Button onClick={exportData}>Download</Button>;
